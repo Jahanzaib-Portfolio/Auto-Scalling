efdd7c9954267e19b4ad217cb1b0ee79b244bf1b
Jahanzaib Khan <jahanzaibkhancloud@gmail.com>
2025-04-26 10:25:37 +0500

    Update auto_scaling_app.py

--- auto_scaling_app.py
+++ auto_scaling_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "This is the Demo application for Auto Scalling Project"
+    return "This is my Demo application for Auto Scalling Project"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
